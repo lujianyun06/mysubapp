122d2337943346ad94e829bd7fc739be0774d64a
lujianyun06 <1903722087@qq.com>
2024-05-08 22:48:01 +0800

    阶段性成果，终于可以成功显示子应用了

--- src/main.js
+++ src/main.js
@@ -1,4 +1,34 @@
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+let instance = null
+
+function render(props = {}) {
+    const { container } = props
+    // 为了避免根id#app与其他DOM冲突，需要限制查找范围
+    console.log(container)
+    let appdom = container.querySelector('#app') 
+    console.log(appdom)
+    instance = createApp(App).use(router).mount(container ? container.querySelector('#app') : '#app')
+
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+    render()
+}
+
+//--------- 生命周期函数------------//
+export async function bootstrap() {
+    console.log('[vue] vue app bootstraped')
+}
+export async function mount(props) {
+    console.log('[vue] props from main framework', props)
+    render(props)
+}
+export async function unmount() {
+    instance.$destroy()
+    instance.$el.innerHTML = ''
+    instance = null
+}
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { name } = require('./package');
+module.exports = {
+    devServer: {
+        port: 8083,			// 重点6
+        headers: {			// 重点7：同重点1，允许子应用跨域
+            'Access-Control-Allow-Origin': '*',
+        },
+    },
+    // 自定义webpack配置
+    configureWebpack: {
+        output: {
+            library: `${name}-[name]`,
+            libraryTarget: 'umd',		// 把子应用打包成 umd 库格式
+        },
+        
+    },
+};
